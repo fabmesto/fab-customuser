c45c3bfd5fe58f58ee8bce23491265e4a077c144
Fabrizio Mesto <fabrizio.mesto@telntsrl.com>
2017-11-22 14:46:49 +0100

    fix

--- fab-customuser.php
+++ fab-customuser.php
@@ -29,43 +29,48 @@ class Fab_Custom_User {
 		add_filter( 'the_content', array( &$this, 'show_ads_in_content') );
 	}
 
-		/* MOSTRA ADSENSE */
-		public function show_extra_profile_fields( $user ) {
-			if( array_intersect($this->allowed_roles, $user->roles ) ) {
-				?>
+	/* MOSTRA ADSENSE */
+	public function show_extra_profile_fields( $user ) {
+		if( array_intersect($this->allowed_roles, $user->roles ) ) {
+			?>
 
-				<h3>Informazioni riservate agli autori </h3>
-				<table class="form-table">
-					<tr>
-						<th><label for="adsense">ADSENSE Code</label></th>
-						<td>
-							<textarea name="adsense" id="adsense" class="regular-text" rows="5" cols="30"><?php echo esc_attr( get_the_author_meta( 'adsense', $user->ID ) ); ?></textarea><br />
-							<span class="description">Codice ADSENSE che apparirà in ogni articolo dell'autore</span>
-						</td>
-					</tr>
-				</table>
-			<?php }
-		}
-
-		/* SALVA ADSENSE */
-		public function save_extra_profile_fields( $user_id ) {
-
-			if ( !current_user_can( 'edit_user', $user_id ) )
-			return false;
-
-			update_usermeta( $user_id, 'adsense', $_POST['adsense'] );
-		}
-
-		public function setupAdminMenus() {
-			//add_menu_page( 'FAB Settings', 'FAB plugin', 'manage_options', 'fab_settings', array( &$this, 'settingsPage' ) );
-			//add_menu_page( 'FAB Test SB', 'FAB test DB', 'manage_options', 'fab_test_db', array( &$this, 'testDB' ) );
-		}
-
-		public function show_ads_in_content($content) {
-			$adsense = '<div class="adsense-user text-center">'.get_the_author_meta( 'adsense' ).'</div>';
-			return $adsense.$content.$adsense;
-		}
-
+			<h3>Informazioni riservate agli autori </h3>
+			<table class="form-table">
+				<tr>
+					<th><label for="adsense">ADSENSE Code</label></th>
+					<td>
+						<textarea name="adsense" id="adsense" class="regular-text" rows="5" cols="30"><?php echo esc_attr( get_the_author_meta( 'adsense', $user->ID ) ); ?></textarea><br />
+						<span class="description">Codice ADSENSE che apparirà in ogni articolo dell'autore (TOP and BOTTOM)</span>
+					</td>
+				</tr>
+			</table>
+		<?php }
 	}
 
-	$fab_custom_user = new Fab_Custom_User();
+	/* SALVA ADSENSE */
+	public function save_extra_profile_fields( $user_id ) {
+
+		if ( !current_user_can( 'edit_user', $user_id ) )
+		return false;
+
+		update_usermeta( $user_id, 'adsense', $_POST['adsense'] );
+	}
+
+	public function setupAdminMenus() {
+		//add_menu_page( 'FAB Settings', 'FAB plugin', 'manage_options', 'fab_settings', array( &$this, 'settingsPage' ) );
+		//add_menu_page( 'FAB Test SB', 'FAB test DB', 'manage_options', 'fab_test_db', array( &$this, 'testDB' ) );
+	}
+
+	public function show_ads_in_content($content) {
+		$adsense = get_the_author_meta( 'adsense' );
+		if($adsense!=''){
+			$code = '<div class="adsense-user text-center">'.$adsense.'</div>';
+		}else{
+			$code = '<!-- NO ADSENSE AUTORE -->';
+		}
+		return $code.$content.$code;
+	}
+
+}
+
+$fab_custom_user = new Fab_Custom_User();
